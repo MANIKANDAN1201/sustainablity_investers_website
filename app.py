e97af221f70894a846e6a7f7592cd57cde62ced4
Manikandan K <140843246+MANIKANDAN1201@users.noreply.github.com>
2024-05-09 19:37:15 +0000

    added dynamic data and json

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify,render_template
 app=Flask(__name__)
+
+INVEST=[
+    {
+        "name":"go green",
+        "fund":"$100,000",
+        "stock_available":'10%'
+    },
+    {
+        "name":"life saver",
+        "fund":"$120,000",
+        "stock_available":'20%'
+    }
+]
 @app.route("/")
 def demo():
-  return render_template('home.html')
+  return render_template('home.html',invest=INVEST,name="ChangeForge")
+@app.route("/api/invest")
+def list_invest():
+  return jsonify(INVEST)
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
